090b55e2128e21c89a12ca57eebe9bcec6ce4e0b
Joaquin Cardenas <jcardenas.lie@gmail.com>
2019-12-20 00:47:58 -0300

    Add exceptions and some functions

---
+++ HangmanFolder/Game.java
@@ -0,0 +1,64 @@
+class Game{
+    public static final int MAX_MISSES = 7;
+    private String answer;
+    private String hits;
+    private String misses;
+
+    public Game(String answer){
+        this.answer = answer.toLowerCase();
+        hits = "";
+        misses = "";
+    }
+
+    private char normalizedGuess(char letter){
+        if (! Character.isLetter(letter)) throw new IllegalArgumentException("A letter is required");
+        letter = Character.toLowerCase(letter);
+        if (isInMisses(letter) || isInHits(letter)) throw new IllegalArgumentException(letter + " has already been guessed");
+        return letter;
+    }
+
+    private boolean isInMisses(char letter){
+        return misses.indexOf(letter) != -1;
+    }
+
+    private boolean isInHits(char letter){
+        return hits.indexOf(letter) != -1;
+    }
+
+    private boolean isInAnswer(char letter){
+        return answer.indexOf(letter) != -1;
+    }
+
+    public boolean applyGuess(char letter){
+        letter = normalizedGuess(letter);
+        boolean isHit = isInAnswer(letter);
+        if (isHit){
+            hits += letter;
+        } else {
+            misses += letter;
+        }
+        return isHit;
+    }
+
+    public boolean applyGuess(String letters){
+        if (letters.length() == 0) throw new IllegalArgumentException("No letter found");
+        return applyGuess(letters.charAt(0));
+
+    }
+    
+    public int getRemainingTries(){
+        return MAX_MISSES - misses.length();
+    }
+
+    public String getCurrentProgress(){
+        String progress = "";
+        for (char letter : answer.toCharArray()){
+            char display = '-';
+            if (hits.indexOf(letter) != -1){
+                display = letter;
+            }
+            progress += display;
+        }
+        return progress;
+    }
+}
---
+++ HangmanFolder/Prompter.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Prompter {
+    private Game game;
+
+    public Prompter(Game game) {
+        this.game = game;
+    }
+
+    public boolean promptForGuess() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isHit = false;
+        boolean isAcceptable = false;
+
+        System.out.println("Enter a letter: ");
+        if (scanner.hasNextLine()) {
+            do {
+                String guessInput = scanner.nextLine();
+                try {
+                    isHit = game.applyGuess(guessInput);
+                    isAcceptable = true;
+                } catch (IllegalArgumentException e) {
+                    System.out.printf("%s. Please try again. %n", e.getMessage());
+                }
+
+                return isHit;
+            } while (!isAcceptable);
+        }
+        System.out.println("No line");
+        return false;
+    }
+
+    public void displayProgress() {
+        System.out.printf("You have %d remaining tries to solve: %s%n", game.getRemainingTries(),
+                game.getCurrentProgress());
+    }
+}
